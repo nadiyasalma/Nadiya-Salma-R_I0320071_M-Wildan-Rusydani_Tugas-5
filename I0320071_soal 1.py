9594f69d634b3b637838d2e3aa5a1375524cae01
nadiyasalma <nadiyasalma084@gmail.com>
2021-04-01 21:07:24 +0700

    tugas 5 nadiya

---
+++ I0320071_soal 1.py
@@ -0,0 +1,9 @@
+print('masukkan nama anda!')
+a = input('nama: ')
+print('masukkan jenis kelamin anda!')
+b = input('jenis kelamin (p/l): ')
+
+if a == 'l':
+    print("halo selamat datang, tuan", a, "!")
+else:
+    print("halo selamat datang, nyonya", b, "!")
